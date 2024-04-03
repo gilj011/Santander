2188f10c25e9ce07bfee39b7cf1e812052e63c32
gilj011 <gilbertoearn@gmail.com>
2024-04-03 11:03:39 -0300

    Projeto Criado

---
+++ santander-dev/src/main/java/com/junior/santanderdev/SantanderDevApplication.java
@@ -0,0 +1,13 @@
+package com.junior.santanderdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevApplication.class, args);
+	}
+
+}
